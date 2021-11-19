1639f2aedb999aeef168fc8f7c718d55fcbe7470
George Makridakis <george.makridakis@orfium.com>
2021-11-19 17:56:44 +0200

    Initialize Admin pages

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from listings.models import Listing
 
-# Register your models here.
+admin.site.register(Listing)
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from listings.models import Realtor
 
-# Register your models here.
+admin.site.register(Realtor)
