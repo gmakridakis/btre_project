b7e1fcd9b8b9f27180e8aa7d659cc913a97e1b3d
George Makridakis <george.makridakis@orfium.com>
2022-01-04 13:38:02 +0200

    Listings page

--- listings/admin.py
+++ listings/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from listings.models import Listing
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("title", "is_published", "price")
+    list_filter = ("title", "is_published")
+    #list_editable = ("title","is_published")
+    search_fields=("title", "is_published", "state")
+    #list_display_links = ()
+    list_per_page=5
+
+
+admin.site.register(Listing, ListingAdmin)
--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,17 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-
+from listings.models import Listing
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.order_by("-price")
+    paginator = Paginator(listings, 3)
+    page = request.GET.get("page")
+    paginated_listing = paginator.get_page(page)
+    context = {"listings": paginated_listing}
 
-def listing(request):
+    return render(request, 'listings/listings.html', context)
+
+def listing(request ,listing_id):
     return render(request, 'listings/listing.html')
 
 def search(request):
