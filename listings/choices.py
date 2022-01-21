65332878da19b6559ff8fa32a138c2ae05609a53
George Makridakis <george.makridakis@orfium.com>
2022-01-21 16:12:10 +0200

    Search form choices

---
+++ listings/choices.py
@@ -0,0 +1,11 @@
+bedroom_choices = {
+    "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8
+}
+
+
+state_choices = {"CA": "California", "NY": "New York"}
+
+price_choices = {
+        "100000": "$100,000",
+        "200000": "$200,000"
+    }
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from listings.choices import bedroom_choices, state_choices, price_choices
+
 def index(request):
+    listings = Listing.objects.all().order_by("-list_date").filter(is_published=True)
+    context = {
+        "listings": listings,
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices
+    }
+
     return render(request, "pages/index.html")
 
 def about(request):
