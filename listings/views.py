790dd4888fb939bc97f9cb2f8ff966090652a7f2
George Makridakis <george.makridakis@orfium.com>
2022-01-24 10:58:28 +0200

    Search page (incomplete)

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
+import keyword
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from listings.models import Listing
+from listings.choices import bedroom_choices, state_choices, price_choices
 
 def index(request):
     listings = Listing.objects.order_by("-price")
@@ -18,4 +20,25 @@ def listing(request ,listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    queryset = Listing.objects.order_by("-list_date")
+
+    if "keywords" in request.GET and request.GET["keywords"]:
+        queryset = queryset.filter(description__icontains=request.GET["keywords"])
+
+
+    if "bedrooms" in request.GET and request.GET["bedrooms"]:
+        queryset = queryset.filter(bedrooms__lte=request.GET["bedrooms"])
+
+
+
+    listings = queryset
+
+    context = {
+        "listings": listings,
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices
+    }
+
+    return render(request, 'listings/search.html', context)
