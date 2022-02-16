bdaca386a5a521d4ad3b443424c9816f67b5420f
George Makridakis <george.makridakis@orfium.com>
2022-02-16 17:51:21 +0200

    Initialize Contact form

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from contacts.models import Contact
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "contact_date", "listing")
+    search_fields = ("name", "email", "listing")
+
+    list_per_page = 37
+
+
+admin.site.register(Contact, ContactAdmin)
